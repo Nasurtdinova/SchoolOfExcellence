2a07d17d152e7700e6c4d71e70abd9881b725046
Nasurtdinova <nasurtdinovag@mail.ru>
2022-11-09 19:34:15 +0300

    Update

--- SchoolOfExcellence/ReportsPage.xaml.cs
+++ SchoolOfExcellence/ReportsPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BespokeFusion;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace SchoolOfExcellence
 {
@@ -46,6 +48,83 @@ namespace SchoolOfExcellence
                 dgPopularDestinations.Visibility = Visibility.Collapsed;
                 dgCountSubject.Visibility = Visibility.Visible;
             }
+        }
+
+        private void btnExcel_Click(object sender, RoutedEventArgs e)
+        {
+            if (comboTypeReport.SelectedItem != null && comboTypeReport.SelectedIndex == 0)
+            {             
+                Excel.Application application = new Excel.Application();
+                Excel.Workbook workbook = application.Workbooks.Add(Type.Missing);
+                Excel.Worksheet worksheet = application.Worksheets.Item[1];
+                int startRowIndex = 1;
+
+                worksheet.Cells[1][startRowIndex] = "Название";
+                worksheet.Cells[2][startRowIndex] = "Количество человек";
+                startRowIndex++;
+
+                var results = DataAccess.GetActivities().OrderByDescending(a => a.Count);
+                foreach (var result in results)
+                {
+                    worksheet.Cells[1][startRowIndex] = result.Name;
+                    worksheet.Cells[2][startRowIndex] = result.Count;
+                    startRowIndex++;
+                }
+                worksheet.Columns.AutoFit();
+                worksheet.Rows.AutoFit();
+                startRowIndex = 1;
+                application.Visible = true;
+            }
+            else if (comboTypeReport.SelectedItem != null && comboTypeReport.SelectedIndex == 1)
+            {
+                Excel.Application application = new Excel.Application();
+                Excel.Workbook workbook = application.Workbooks.Add(Type.Missing);
+                Excel.Worksheet worksheet = application.Worksheets.Item[1];
+                int startRowIndex = 1;
+
+                worksheet.Cells[1][startRowIndex] = "ФИО";
+                worksheet.Cells[2][startRowIndex] = "Класс";
+                worksheet.Cells[2][startRowIndex] = "Количество кружков";
+                startRowIndex++;
+
+                var results = DataAccess.GetStudents().OrderByDescending(a => a.CountActivity);
+                foreach (var result in results)
+                {
+                    worksheet.Cells[1][startRowIndex] = result.FullName;
+                    worksheet.Cells[2][startRowIndex] = result.Grade.Name;
+                    worksheet.Cells[2][startRowIndex] = result.CountActivity;
+                    startRowIndex++;
+                }
+                worksheet.Columns.AutoFit();
+                worksheet.Rows.AutoFit();
+                startRowIndex = 1;
+                application.Visible = true;
+            }
+            else if (comboTypeReport.SelectedItem != null && comboTypeReport.SelectedIndex == 3)
+            {
+                Excel.Application application = new Excel.Application();
+                Excel.Workbook workbook = application.Workbooks.Add(Type.Missing);
+                Excel.Worksheet worksheet = application.Worksheets.Item[1];
+                int startRowIndex = 1;
+
+                worksheet.Cells[1][startRowIndex] = "ФИО";
+                worksheet.Cells[2][startRowIndex] = "Количество кружков";
+                startRowIndex++;
+
+                var results = DataAccess.GetTeachers().OrderByDescending(a => a.CountSubject);
+                foreach (var result in results)
+                {
+                    worksheet.Cells[1][startRowIndex] = result.User.FullName;
+                    worksheet.Cells[2][startRowIndex] = result.CountSubject;
+                    startRowIndex++;
+                }
+                worksheet.Columns.AutoFit();
+                worksheet.Rows.AutoFit();
+                startRowIndex = 1;
+                application.Visible = true;
+            }
+            else
+                MaterialMessageBox.ShowError("Выберите тип!", "Предупреждение");
         }
     }
 }
--- SchoolOfExcellence/Windows/AddScheduleWindow.xaml.cs
+++ SchoolOfExcellence/Windows/AddScheduleWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using SchoolOfExcellence.Database;
+﻿using BespokeFusion;
+using SchoolOfExcellence.Database;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,21 +25,31 @@ namespace SchoolOfExcellence
             comboActivity.ItemsSource = DataAccess.GetActivities();
             comboCabinet.ItemsSource = DataAccess.GetCabinets();
             comboTeachers.ItemsSource = DataAccess.GetTeachers();
+            dpDate.DisplayDateStart = DateTime.Now;
         }
 
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
-            Schedule sch = new Schedule()
+            if (DataAccess.GetSchedules().Where(a => a.Cabinet == comboCabinet.SelectedItem as Cabinet && a.LessonStartTime == tbLessonStart.SelectedTime.Value.TimeOfDay && a.LessonEndTime >= tbLessonEnd.SelectedTime.Value.TimeOfDay).Count() == 0)
             {
-                TeacherActivity = DataAccess.GetTeachersActivities().Where(a => a.Activity == comboActivity.SelectedItem as Activity && a.Teacher == comboTeachers.SelectedItem as Teacher).FirstOrDefault(),
-                Cabinet = comboCabinet.SelectedItem as Cabinet,
-                Date =dpDate.SelectedDate,
-                IsSkipped = false,
-                LessonEndTime = tbLessonEnd.SelectedTime.Value.TimeOfDay,
-                LessonStartTime = tbLessonStart.SelectedTime.Value.TimeOfDay            
-            };
-            DataAccess.AddSchedule(sch);
-            MessageBox.Show("Информация сохранена!");
+                Schedule sch = new Schedule()
+                {
+                    TeacherActivity = DataAccess.GetTeachersActivities().Where(a => a.Activity == comboActivity.SelectedItem as Activity && a.Teacher == comboTeachers.SelectedItem as Teacher).FirstOrDefault(),
+                    Cabinet = comboCabinet.SelectedItem as Cabinet,
+                    Date = dpDate.SelectedDate,
+                    IsSkipped = false,
+                    LessonEndTime = tbLessonEnd.SelectedTime.Value.TimeOfDay,
+                    LessonStartTime = tbLessonStart.SelectedTime.Value.TimeOfDay
+                };
+                DataAccess.AddSchedule(sch);
+                MaterialMessageBox.Show("Информация сохранена!");
+            }
+            else if (DataAccess.GetSchedules().Where(a => a.TeacherActivity == DataAccess.GetTeachersActivities().Where(b => b.Activity == comboActivity.SelectedItem as Activity && b.Teacher == comboTeachers.SelectedItem as Teacher).FirstOrDefault()).Count() != 0)
+                MaterialMessageBox.ShowError("У вас в это время другая группа!");
+            else
+                MaterialMessageBox.ShowError("В этом кабинете в это время проводится другой кружок!");
+            
+            
         }
 
         private void comboTeachers_SelectionChanged(object sender, SelectionChangedEventArgs e)
